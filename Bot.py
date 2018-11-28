d7865e5231c171b74ff39dfaccb12390b262e9b3
Kysan721 <kyshu.senpai@gmail.com>
2018-11-28 16:57:00 +0100

    ajoute très très très mineurs

--- Bot.py
+++ Bot.py
@@ -2,4 +2,5 @@ import IrcClient
 
 
 class Bot(IrcClient.IrcClient):
-    def __init__(self, )
+    def __init__(self, nick, hostname, chan, port=6667, senpai)):
+        IrcClient.IrcClient(nick, hostname, chan)
